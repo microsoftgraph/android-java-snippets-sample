a7473e7b96b69720f645d1b24087d9b6babe8c9d
Ricardo Loo <ricardol@microsoft.com>
2016-05-26 13:01:23 -0700

    Added a UI test to verify that the Azure AD sign-in page loads

--- app/src/androidTest/java/com/microsoft/graph/snippets/SignInActivityTests.java
+++ app/src/androidTest/java/com/microsoft/graph/snippets/SignInActivityTests.java
@@ -1,7 +1,28 @@
+/*
+ * Copyright (c) Microsoft. All rights reserved. Licensed under the MIT license.
+ * See LICENSE in the project root for license information.
+ */
 package com.microsoft.graph.snippets;
 
-/**
- * Created by ricalo on 5/25/16.
- */
+import android.support.test.espresso.intent.rule.IntentsTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+
+@RunWith(AndroidJUnit4.class)
 public class SignInActivityTests {
+    @Rule
+    public IntentsTestRule<SignInActivity> mActivityRule = new IntentsTestRule<>(SignInActivity.class);
+
+    @Test
+    public void displayAzureADSignIn() {
+        ServiceConstants.CLIENT_ID = "12345678-1234-1234-1234-1234567890ab";
+        onView(withId(R.id.o365_signin)).perform(click());
+    }
 }
