8b2764c2004281d97884a89a52625fe33bacc834
Ricardo Loo <ricardol@microsoft.com>
2016-08-09 13:00:55 -0700

    Replaced thread.sleep by idling resource that informs the test...
    when the app is done executing HTTP requests

--- app/src/androidTest/java/com/microsoft/graph/snippets/SnippetTests.java
+++ app/src/androidTest/java/com/microsoft/graph/snippets/SnippetTests.java
@@ -4,7 +4,10 @@
  */
 package com.microsoft.graph.snippets;
 
+import android.content.Intent;
+import android.support.test.espresso.IdlingResource;
 import android.support.test.espresso.intent.rule.IntentsTestRule;
+import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.widget.ListView;
 
@@ -14,8 +17,13 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static android.support.test.espresso.Espresso.onData;
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.Espresso.registerIdlingResources;
+import static android.support.test.espresso.Espresso.unregisterIdlingResources;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.intent.Intents.intended;
@@ -32,11 +40,12 @@ import static org.hamcrest.core.IsAnything.anything;
 public class SnippetTests {
     @Rule
     public IntentsTestRule<SnippetListActivity> mSnippetListActivityRule = new IntentsTestRule<>(SnippetListActivity.class);
+    @Rule
+    public ActivityTestRule<SnippetDetailActivity> mSnippetDetailActivityRule = new ActivityTestRule<>(SnippetDetailActivity.class, false, false);
 
     @Test
     public void RunSnippets() throws InterruptedException{
-        SnippetListActivity snippetListActivityActivity = mSnippetListActivityRule.getActivity();
-        int numItems = ((ListView) snippetListActivityActivity
+        int numItems = ((ListView) mSnippetListActivityRule.getActivity()
                 .getSupportFragmentManager()
                 .findFragmentById(R.id.snippet_list)
                 .getView()
@@ -44,7 +53,11 @@ public class SnippetTests {
                 .getAdapter()
                 .getCount();
 
-        for (int i = 0; i < numItems; i++){
+        List<Integer> snippetIndexes = new ArrayList<>();
+
+        // Get the index of items in the adapter that
+        // are actual snippets and not headers
+        for (int i = 0; i < numItems; i++) {
             onData(anything())
                     .inAdapterView(withId(android.R.id.list))
                     .atPosition(i)
@@ -56,17 +69,55 @@ public class SnippetTests {
                         hasExtra("item_id", i),
                         toPackage("com.microsoft.graph.snippets")
                 ));
+                snippetIndexes.add(i);
+                onView(withContentDescription("Navigate up")).perform(click());
             } catch (AssertionFailedError e) {
                 continue;
             }
+        }
+
+        // Execute the snippets
+        for(int index : snippetIndexes) {
+            Intent itemIdIntent = new Intent();
+            itemIdIntent.putExtra("item_id", index);
+            SnippetDetailActivity snippetDetailActivity = mSnippetDetailActivityRule.launchActivity(itemIdIntent);
+
+            SnippetDetailActivityIdlingResource idlingResource = new SnippetDetailActivityIdlingResource(snippetDetailActivity);
+            registerIdlingResources(idlingResource);
 
             onView(withId(R.id.btn_run)).perform(click());
 
-            Thread.sleep(3000, 0);
-
             onView(withId(R.id.txt_status_color)).check(matches(withContentDescription(R.string.stoplight_success)));
+            unregisterIdlingResources(idlingResource);
+            snippetDetailActivity.finish();
+        }
+    }
 
-            onView(withContentDescription("Navigate up")).perform(click());
+    private class SnippetDetailActivityIdlingResource implements IdlingResource {
+        private SnippetDetailActivity activity;
+        private ResourceCallback callback;
+
+        public SnippetDetailActivityIdlingResource(SnippetDetailActivity activity) {
+            this.activity = activity;
+        }
+
+        @Override
+        public String getName() {
+            return this.getClass().getName();
+        }
+
+        @Override
+        public boolean isIdleNow() {
+            Boolean idle = activity != null &&
+                    callback != null &&
+                    !activity.isExecutingRequest();
+            if (idle) callback.onTransitionToIdle();
+            return idle;
+        }
+
+        @Override
+        public void registerIdleTransitionCallback(ResourceCallback resourceCallback) {
+            this.callback = resourceCallback;
         }
     }
 }
