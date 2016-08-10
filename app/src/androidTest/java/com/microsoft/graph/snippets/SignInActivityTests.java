0df2a6b4a1862a2fa98ce1ace84fb355ee365a94
Ricardo Loo <ricardol@microsoft.com>
2016-08-10 09:38:13 -0700

    Added test for disconnect action

--- app/src/androidTest/java/com/microsoft/graph/snippets/SignInActivityTests.java
+++ app/src/androidTest/java/com/microsoft/graph/snippets/SignInActivityTests.java
@@ -5,6 +5,7 @@
 package com.microsoft.graph.snippets;
 
 import android.os.Environment;
+import android.support.test.InstrumentationRegistry;
 import android.support.test.espresso.NoMatchingViewException;
 import android.support.test.espresso.intent.rule.IntentsTestRule;
 import android.support.test.espresso.web.webdriver.DriverAtoms;
@@ -24,12 +25,14 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.intent.Intents.intended;
 import static android.support.test.espresso.intent.matcher.ComponentNameMatchers.hasShortClassName;
 import static android.support.test.espresso.intent.matcher.IntentMatchers.hasComponent;
 import static android.support.test.espresso.intent.matcher.IntentMatchers.toPackage;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static android.support.test.espresso.web.sugar.Web.onWebView;
 import static android.support.test.espresso.web.webdriver.DriverAtoms.clearElement;
 import static android.support.test.espresso.web.webdriver.DriverAtoms.findElement;
@@ -38,57 +41,96 @@ import static org.hamcrest.core.AllOf.allOf;
 
 @RunWith(AndroidJUnit4.class)
 public class SignInActivityTests {
-    private static String testClientId;
+    private static final String USER_ID_TEXT_ELEMENT = "cred_userid_inputtext";
+    private static final String PASSWORD_TEXT_ELEMENT = "cred_password_inputtext";
+    private static final String SIGN_IN_BUTTON_ELEMENT = "cred_sign_in_button";
+    private static final String TEST_ARTIFACT_LOCATION = "local/testConfig.json";
+    private static final String CLIENT_ID_TEST_ARTIFACT = "test_client_id";
+    private static final String USERNAME_TEST_ARTIFACT = "test_username";
+    private static final String PASSWORD_TEST_ARTIFACT = "test_password";
     private static String testUsername;
     private static String testPassword;
 
     @Rule
-    public IntentsTestRule<SignInActivity> mSignInActivityRule = new IntentsTestRule<>(SignInActivity.class);
+    public IntentsTestRule<SignInActivity> mSignInActivityRule = new IntentsTestRule<>(SignInActivity.class, false, false);
+    @Rule
+    public IntentsTestRule<SnippetListActivity> mSnippetListActivityRule = new IntentsTestRule<>(SnippetListActivity.class, false, false);
 
     @BeforeClass
     public static void getTestParameters() throws FileNotFoundException {
-        File testConfigFile = new File(Environment.getDataDirectory(), "local/testConfig.json");
+        File testConfigFile = new File(Environment.getDataDirectory(), TEST_ARTIFACT_LOCATION);
         JsonObject testConfig = new JsonParser().parse(new FileReader(testConfigFile)).getAsJsonObject();
-        testClientId = testConfig.get("test_client_id").getAsString();
-        testUsername = testConfig.get("test_username").getAsString();
-        testPassword = testConfig.get("test_password").getAsString();
-        ServiceConstants.CLIENT_ID = testClientId;
+        testUsername = testConfig.get(USERNAME_TEST_ARTIFACT).getAsString();
+        testPassword = testConfig.get(PASSWORD_TEST_ARTIFACT).getAsString();
+        ServiceConstants.CLIENT_ID = testConfig.get(CLIENT_ID_TEST_ARTIFACT).getAsString();
     }
 
     @Test
     public void AzureADSignIn() throws InterruptedException{
+        AzureADSignIn(testUsername, testPassword, mSignInActivityRule);
+    }
+
+    @Test
+    public void Disconnect() {
+        Disconnect(mSnippetListActivityRule);
+    }
+
+    public static void AzureADSignIn(String username, String password, IntentsTestRule<SignInActivity> signInActivityTestRule) throws InterruptedException {
+        SignInActivity signInActivity = signInActivityTestRule.launchActivity(null);
+
         onView(withId(R.id.o365_signin)).perform(click());
 
         try {
             onWebView()
-                    .withElement(findElement(Locator.ID, "cred_userid_inputtext"))
+                    .withElement(findElement(Locator.ID, USER_ID_TEXT_ELEMENT))
                     .perform(clearElement())
                     // Enter text into the input element
-                    .perform(DriverAtoms.webKeys(testUsername))
+                    .perform(DriverAtoms.webKeys(username))
                     // Set focus on the username input text
                     // The form validates the username when this field loses focus
                     .perform(webClick())
-                    .withElement(findElement(Locator.ID, "cred_password_inputtext"))
+                    .withElement(findElement(Locator.ID, PASSWORD_TEXT_ELEMENT))
                     // Now we force focus on this element to make
                     // the username element to lose focus and validate
                     .perform(webClick())
                     .perform(clearElement())
                     // Enter text into the input element
-                    .perform(DriverAtoms.webKeys(testPassword));
+                    .perform(DriverAtoms.webKeys(password));
 
             Thread.sleep(2000, 0);
 
             onWebView()
-                    .withElement(findElement(Locator.ID, "cred_sign_in_button"))
+                    .withElement(findElement(Locator.ID, SIGN_IN_BUTTON_ELEMENT))
                     .perform(webClick());
         } catch (NoMatchingViewException ex) {
             // If user is already logged in, the flow will go directly to SnippetListActivity
+        } finally {
+            Thread.sleep(2000, 0);
         }
 
-        Thread.sleep(2000, 0);
+        // Finally, verify that SnippetListActivity is on top
         intended(allOf(
                 hasComponent(hasShortClassName(".SnippetListActivity")),
                 toPackage("com.microsoft.graph.snippets")
         ));
+
+        signInActivity.finish();
+    }
+
+    public static void Disconnect(IntentsTestRule<SnippetListActivity> snippetListActivityTestRule) {
+        SnippetListActivity snippetListActivity = snippetListActivityTestRule.launchActivity(null);
+
+        openActionBarOverflowOrOptionsMenu(InstrumentationRegistry.getTargetContext());
+
+        // Espresso can't find menu items by id. We'll use the text property.
+        onView(withText(R.string.disconnect_menu_item))
+                .perform(click());
+
+        intended(allOf(
+                hasComponent(hasShortClassName(".SignInActivity")),
+                toPackage("com.microsoft.graph.snippets")
+        ));
+
+        snippetListActivity.finish();
     }
 }
